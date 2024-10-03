eeb64b708774e584379e767f94d17f1999a92ef7
Csenongmin <79391407+Csenongmin@users.noreply.github.com>
2024-10-03 18:25:52 +0900

    수정2차

--- 1394.py
+++ 1394.py
@@ -1,20 +1,18 @@
-import sys
+import sys                 #1394.py 어려웠던 점: 규칙 구현, 시간 초과
 input = sys.stdin.readline
 
 word = input().rstrip()
 password = input().rstrip()
-ans =  0
-MOD = 900528
-last_idx = len(password)-1
-idx = 0
 
-a = 0
+MOD = 900528
+ans =  0
+temp = 0
 for i in range(len(password)):
-    idx = word.index(password[i])+1 #현재 문자 순서 
-    if i > 0:  #전 부분도 계산 필요
-        ans = (a*len(word))%MOD
-    ans += idx %MOD
-    a = ans
+    idx = word.index(password[i])+1     #현재 문자 순서 
+    if i > 0:                           #전 부분도 계산 필요
+        ans = (temp*len(word))%MOD      # MOD 
+    ans += idx % MOD
+    temp = ans
 print(ans%MOD)
         
 
