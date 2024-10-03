8231b28bff5f7bd794023720650f9ff86b2db798
Csenongmin <79391407+Csenongmin@users.noreply.github.com>
2024-10-03 18:23:00 +0900

    1394수정

--- 1394.py
+++ 1394.py
@@ -1,24 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-key = list(input().rstrip())
-password = list(input().rstrip())
+word = input().rstrip()
+password = input().rstrip()
+ans =  0
+MOD = 900528
+last_idx = len(password)-1
+idx = 0
 
-ans=0
-mod = 900528
-len_key = len(key)
-len_password = len(password)
+a = 0
+for i in range(len(password)):
+    idx = word.index(password[i])+1 #현재 문자 순서 
+    if i > 0:  #전 부분도 계산 필요
+        ans = (a*len(word))%MOD
+    ans += idx %MOD
+    a = ans
+print(ans%MOD)
+        
 
-j = 0
-for elem in password:
-    index = word.index(elem)  # 암호 집합에서의 위치
-    if j < length_password-1:   # 전의 경우의수 합
-        ans += length_word ** (j + 1) 
-    
-    if password.index(elem) == length_password - 1:  # 정답 암호중 마지막 암호
-        ans += index+1
-    else: 
-        ans += length_word ** index * index
-    j += 1
 
-print(ans % 900528)
